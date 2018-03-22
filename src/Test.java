43dd0692949215912cc141469771f35be894123e
vignesh071 <vignesh.0791@gmail.com>
2018-03-23 02:28:14 +0530

    reverse

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("test case");
 		
+			
+	
+				 
+					System.out.println("enter the value to reverse");
+					Scanner obj =new Scanner (System.in);
+					String obj2=obj.nextLine();
+					String value= "";
+					for(int x=obj2.length()-1;x>=0;x--)
+					{
+						value=value+obj2.charAt(x);
+						
+					}
+					System.out.println("output");
+					System.out.println(value);
+				}
+					
 	}
-}
+
+
