abbe5e4485fe0654956bdf794a7642e40a5950ea
senthil9790 <sen9790@gmail.com>
2018-03-23 11:17:20 +0530

    Reverse program by senthil

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,18 @@
-
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("test case");
-		System.out.println("test case1");
+		System.out.println("Enter the value for Reverse a String");
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		String reverse=" ";
+		for (int i = s.length()-1;i>=0; i--) {
+			 reverse = reverse+s.charAt(i);
+
+		}
+		System.out.println("The Reversed String is :");
+		System.out.println(reverse);
 	}
 }
