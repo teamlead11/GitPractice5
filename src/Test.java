ee962b2959babe91e77cf5f6d9a0d09b87da897a
PallaviNunna <nunnapallavi12@gmail.com>
2018-03-23 11:27:54 +0530

    new added

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
 
 public class Test {
 
+	public void pallavi() {
+		// TODO Auto-generated method stub
+		System.out.println("enter string to reverse");
+		Scanner s =new Scanner(System.in);
+		String s1=s.nextLine();
+		String reverse="";
+		for(int i=s1.length()-1;i>=0;i--)
+		{
+			reverse=reverse+s1.charAt(i);
+		}
+			
+
+		System.out.println("the reverse string is");
+		System.out.println(reverse);
+		
+		
+		
+
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("test case");
-		
-	}
+		Test t=new Test();
+		t.pallavi();
+		}
 }
