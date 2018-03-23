0ddff9cd69f6edc20de8c336c8f3c8448292b84d
premsri <premsri2504@gmail.com>
2018-03-23 14:51:16 +0530

    commiting reverse string

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,16 @@ public class Test {
 		// TODO Auto-generated method stub
 
 		System.out.println("test case");
-		
+
+	}
+
+	public void premReverse() {
+		String s = "Hello";
+		String reverse = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			reverse = reverse + s.charAt(i);
+		}
+		System.out.println("the reverse string is");
+		System.out.println(reverse);
 	}
 }
