bd5dca21601610b5955c49e3cfcd0e51bda81690
babyrajendran <babyrajendran1994@gmail.com>
2018-03-24 15:16:41 +0530

    Added string reverse program

---
+++ src/ReverseSring_baby.java
@@ -0,0 +1,8 @@
+
+public class ReverseSring_baby {
+	public static void main(String[] args) {
+		//Without using for loop
+		StringBuffer b = new StringBuffer("project");
+		System.out.println(b.reverse());
+	}
+}
